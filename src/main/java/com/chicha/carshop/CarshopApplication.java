deeaa7608fffb551114d6be3738a3d7dcbd99512
Chicha63 <gupal2004@gmail.com>
2024-11-29 09:54:29 +0200

    Initial

--- src/main/java/com/chicha/carshop/CarshopApplication.java
+++ src/main/java/com/chicha/carshop/CarshopApplication.java
@@ -2,10 +2,10 @@ package com.chicha.carshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class CarshopApplication {
-
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class CarshopApplication  {
 	public static void main(String[] args) {
 		SpringApplication.run(CarshopApplication.class, args);
 	}
